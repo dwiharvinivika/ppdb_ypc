7e231af0f798a4aebd097b5e58333ac5b87cdf77
Jangbe <jangebe91@gmail.com>
2021-12-29 07:48:42 +0700

    fix bug sebelumnya

--- resources/views/admin/jurusan/_form.blade.php
+++ resources/views/admin/jurusan/_form.blade.php
@@ -1,46 +1,52 @@
 @csrf
-<div class="item form-group">
-    <label class="col-form-label col-md-3 col-sm-3 label-align"
-        for="kode_jurusan">Kode Jurusan
-    </label>
-    <div class="col-md-6 col-sm-6 ">
-        <input type="text" name="kode_jurusan" id="kode_jurusan"
-            class="form-control @error('kode_jurusan') is-invalid @enderror" value="{{ old('kode_jurusan', $jurusan->kode_jurusan??'') }}">
-            @error('kode_jurusan') <div class="invalid-feedback"> {{ $message }}</div> @enderror
-    </div>
-</div>
-<div class="item form-group">
-    <label class="col-form-label col-md-3 col-sm-3 label-align"
-        for="jurusan">Nama Jurusan
-    </label>
-    <div class="col-md-6 col-sm-6 ">
-        <input type="text" name="jurusan" id="jurusan"
-            class="form-control @error('jurusan') is-invalid @enderror" value="{{ old('jurusan', $jurusan->jurusan??'') }}">
-            @error('jurusan')<div class="invalid-feedback"> {{ $message }}</div> @enderror
-
-    </div>
-</div>
-<div class="item form-group">
-    <label class="col-form-label col-md-3 col-sm-3 label-align"
-        for="gambar">Gambar
-    </label>
-    <div class="col-md-6 col-sm-6 ">
-        <div class="custom-file">
-            <input type="text" name="gambar" id="last-name" name="last-name"
-                class="custom-file-label @error('gambar') is-invalid @enderror" value="{{ old('gambar', $jurusan->gambar??'') }}">
-            <label for="gambar" class="custom-file-label">Pilih Logo</label>
-                @error('gambar')<div class="invalid-feedback"> {{ $message }}</div> @enderror
+<div class="row">
+    <div class="col-md-7">
+        <label class="col-form-label col-md-4 col-sm-3 label-align"
+            for="kode_jurusan">Kode Jurusan
+        </label>
+        <div class="col-md-8 col-sm-6 form-group">
+            <input type="text" name="kode_jurusan" id="kode_jurusan" class="form-control
+            @error('kode_jurusan') is-invalid @enderror" value="{{ old('kode_jurusan', $jurusan->kode_jurusan??'') }}">
+            <x-validation name="kode_jurusan"/>
+        </div>
+        <label class="col-form-label col-md-4 col-sm-3 label-align"
+            for="jurusan">Nama Jurusan
+        </label>
+        <div class="col-md-8 col-sm-6 form-group">
+            <input type="text" name="jurusan" id="jurusan"
+                class="form-control @error('jurusan') is-invalid @enderror" value="{{ old('jurusan', $jurusan->jurusan??'') }}">
+            <x-validation name="jurusan"/>
+            <div class="text-helper"><i>Gunakan <b>&lt;b&gt; huruf &lt;/b&gt;</b> untuk mempertebal huruf/kata</i></div>
+        </div>
+        <label class="col-form-label col-md-4 col-sm-3 label-align"
+            for="gambar">Gambar
+        </label>
+        <div class="col-md-8 col-sm-6 form-group">
+            <div class="custom-file">
+                <input type="file" name="gambar" id="gambar" class="custom-file-input @error('gambar') is-invalid @enderror" accept="image/*">
+                <label for="gambar" class="custom-file-label">Pilih Logo</label>
+                <x-validation name="gambar"/>
+            </div>
+        </div>
+        <label class="col-form-label col-md-4 col-sm-3 label-align"
+            for="keterangan">Keterangan
+        </label>
+        <div class="col-md-8 col-sm-6 form-group">
+            <textarea name="keterangan" id="keterangan"
+                class="form-control @error('keterangan') is-invalid @enderror">{{ old('keterangan', $jurusan->keterangan??'') }}</textarea>
+            <x-validation name="keterangan"/>
         </div>
     </div>
-</div>
-<div class="item form-group">
-    <label class="col-form-label col-md-3 col-sm-3 label-align"
-        for="keterangan">Keterangan
-    </label>
-    <div class="col-md-6 col-sm-6 ">
-        <textarea name="keterangan" id="last-name" name="last-name"
-            class="form-control @error('keterangan') is-invalid @enderror">{{ old('keterangan', $jurusan->keterangan??'') }}</textarea>
-            @error('keterangan')<div class="invalid-feedback"> {{ $message }}</div> @enderror
+    <div class="col-md-4">
+        <img src="{{ asset('img/jurusan/'.($jurusan->gambar??'../default-foto.png')) }}" class="img-thumbnail" id="preview-img" alt="">
     </div>
 </div>
 
+@push('js')
+    <script>
+        $('#gambar').on('change', function(){
+            const [file] = $(this)[0].files;
+            if(file){$('#preview-img').attr('src', URL.createObjectURL(file))}
+        })
+    </script>
+@endpush
